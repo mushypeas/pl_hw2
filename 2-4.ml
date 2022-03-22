4a081275ad8b1ed48bf51464c2468c886cc92184
mushypeas <qkrwoans2@gmail.com>
2022-03-23 00:29:55 +0900

    2-4

--- 2-4.ml
+++ 2-4.ml
@@ -8,14 +8,14 @@ exception EmptyHeap
 let rank h =
   match h with
   | EMPTY -> -1
-  | NODE(r, , , ) -> r
+  | NODE (r,_,_,_) -> r
 
 let insert(x,h) = merge(h, NODE(0,x,EMPTY,EMPTY))
 
 let findMin h =
   match h with
   | EMPTY -> raise EmptyHeap
-  | NODE( ,x, , ) -> x
+  | NODE(_,x,_,_) -> x
 
 let deleteMin h =
   match h with
@@ -28,3 +28,11 @@ let shake (x,lh,rh) =
   else NODE(rank lh + 1, x, rh, lh)
 
 let rec merge: heap * heap -> heap = fun (h1, h2) ->
+  match (h1, h2) with
+  | EMPTY, h -> h
+  | h, EMPTY -> h
+  | NODE (_, x1, a1, b1), NODE (_, x2, a2, b2) ->
+    if (x1 < x2) then
+      shake (x1, a1, (merge b1 h2))
+    else
+      shake (x2, a2, (merge h1 b2))
